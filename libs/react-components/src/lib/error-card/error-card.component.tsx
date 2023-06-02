28ce49d8976c84d6ba93a6cdeb08f74adffd7522
noesparsa <noe.esp@hotmail.fr>
2023-06-02 17:29:28 +0200

    chore: test ci workflow

--- libs/react-components/src/lib/error-card/error-card.component.tsx
+++ libs/react-components/src/lib/error-card/error-card.component.tsx
@@ -18,7 +18,7 @@ export function ErrorCard({ title, children }: IProps): JSX.Element {
   return useMemo(
     (): JSX.Element => (
       <div className="vtmn-container vtmn-flex vtmn-h-screen vtmn-mx-auto">
-        <div className="vtmn-mx-auto gt-tablet:vtmn-my-auto vtmn-p-6">
+        <div className="vtmn-mx-auto gt-tablet:vtmn-my-auto vtmn-p-5">
           <VtmnCard
             title={title}
             variant="top-image"
