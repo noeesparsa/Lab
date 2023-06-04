4c2bc1a5321b899b5fd3d08f023e0a94c149f495
noesparsa <noe.esparsa@decathlon.com>
2023-06-04 20:15:02 +0200

    chore: fix code smells

--- apps/react-lab/src/app/components/router-global-error/router-global-error.component.tsx
+++ apps/react-lab/src/app/components/router-global-error/router-global-error.component.tsx
@@ -2,33 +2,36 @@ import React, { ReactNode, useMemo } from 'react';
 import { isRouteErrorResponse, useRouteError } from 'react-router-dom';
 import { ErrorCard } from '@laboratory/react-components';
 
+function generateErrorMessage(error: unknown): ReactNode {
+  if (!isRouteErrorResponse(error)) {
+    return ``;
+  }
+  const { status, statusText, data } = error;
+
+  return (
+    <>
+      <p>
+        <b>Error {status}</b>
+        {statusText && `: ${statusText}`}
+      </p>
+
+      {typeof data === 'string' && (
+        <p className="vtmn-typo_caption-1 linqueo-color-grey">{data}</p>
+      )}
+
+      {typeof data === 'object' && (
+        <pre>
+          <code>{JSON.stringify(data, null, 2)}</code>
+        </pre>
+      )}
+    </>
+  );
+}
+
 export default function RouterGlobalError(): React.ReactElement {
   const error: unknown = useRouteError();
 
-  let children: ReactNode = ``;
-
-  if (isRouteErrorResponse(error)) {
-    const { status, statusText, data } = error;
-
-    children = (
-      <>
-        <p>
-          <b>Error {status}</b>
-          {statusText && `: ${statusText}`}
-        </p>
-
-        {typeof data === 'string' && (
-          <p className="vtmn-typo_caption-1 linqueo-color-grey">{data}</p>
-        )}
-
-        {typeof data === 'object' && (
-          <pre>
-          <code>{JSON.stringify(data, null, 2)}</code>
-        </pre>
-        )}
-      </>
-    );
-  }
+  const children: ReactNode = generateErrorMessage(error);
 
   return useMemo(
     (): React.ReactElement => (
