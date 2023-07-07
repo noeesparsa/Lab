3eed34d63cd9e86df61dd0c93f3e55a68e314a4c
noesparsa <noe.esparsa@decathlon.com>
2023-07-07 23:08:35 +0200

    chore: update router

--- apps/react-lab/src/app/constants/routes.tsx
+++ apps/react-lab/src/app/constants/routes.tsx
@@ -1,19 +1,18 @@
-import { RouteObject } from 'react-router-dom';
+import {RouteObject} from 'react-router-dom';
 import App from '../app';
-import { HomePage } from '../pages/home/home-page';
+import {HomePage} from '../pages/home/home-page';
 import RouterGlobalError from '../components/router-global-error/router-global-error.component';
 
 export const routes: RouteObject[] = [
   {
     id: 'root',
-    path: '/',
     element: <App />,
     errorElement: <RouterGlobalError />,
 
     children: [
       {
         id: 'home',
-        path: 'home',
+        path: '/',
         element: <HomePage />,
       },
     ],
