7721c8a92f4628fc05b356f3015cf5ad79f641ad
noesparsa <noe.esp@hotmail.fr>
2023-07-11 15:18:22 +0200

    chore: add  stylelint

--- .lintstagedrc.cjs
+++ .lintstagedrc.cjs
@@ -1,6 +1,14 @@
 module.exports = {
-  '*.{js,ts,html,cjs,mjs,jsx,tsx,md,json,yml}'(files) {
-    return [`nx affected:lint --files=${files.join(',')}`,`nx format:write --files=${files.join(',')}`, 'nx affected --target=test'];
+  '*.{md,json,yml}'(files){
+    return [`nx format:write --files=${files.join(',')}`]
+  },
+
+  '*.{scss}'() {
+    return ['nx affected --target=stylelint']
+  },
+
+  '*.{js,ts,html,cjs,mjs,jsx,tsx}'(files) {
+    return [`nx affected:lint --files=${files.join(',')}`, 'nx affected --target=test'];
   },
 };
 
