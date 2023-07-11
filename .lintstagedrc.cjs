1d558070edae1c79013ce62aa48d5aebe432317c
Noé Esparsa <35199957+Rangoow@users.noreply.github.com>
2023-07-11 23:19:43 +0200

    chore: configure stylelint lintstaged and commitlint (#55)

---
+++ .lintstagedrc.cjs
@@ -0,0 +1,14 @@
+module.exports = {
+  '*.{md,json,yml}'(files){
+    return [`nx format:write --files=${files.join(',')}`]
+  },
+
+  '*.{scss}'() {
+    return ['nx affected --target=stylelint']
+  },
+
+  '*.{js,ts,html,cjs,mjs,jsx,tsx}'(files) {
+    return [`nx affected:lint --files=${files.join(',')}`, 'nx affected --target=test'];
+  },
+};
+
