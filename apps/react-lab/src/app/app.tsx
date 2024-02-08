478158b1e4e09fd18060979690ca21b96c0978ad
Rangoow <noe.esp@hotmail.fr>
2024-02-08 17:00:05 +0100

    chore: add link to navbar + remove unused sidebar buttons

--- apps/react-lab/src/app/app.tsx
+++ apps/react-lab/src/app/app.tsx
@@ -1,6 +1,7 @@
-import { VtmnButton } from '@vtmn/react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import classNames from 'classnames/bind';
 import { Outlet } from 'react-router-dom';
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 import styles from './app.module.scss';
 import { Navbar, SidebarMenu } from '@lab/react-components';
 
@@ -12,21 +13,31 @@ export default function App(): React.JSX.Element {
         <Navbar
           logoUrl="/logo.png"
           buttons={[
-            <VtmnButton key={1} size={'medium'} iconAlone="home-fill" />,
-            <VtmnButton key={2} size={'medium'} iconAlone="user-fill" />,
+            <a
+              key={1}
+              href="https://github.com/Rangoow/Lab"
+              target="_blank"
+              rel="noopener noreferrer"
+              className={cx('skeleton__header__link')}
+            >
+              <FontAwesomeIcon key={1} size="lg" icon={faGithub} />
+            </a>,
+            <a
+              key={2}
+              href="https://www.linkedin.com/in/no%C3%A9-esparsa-22557213a/"
+              target="_blank"
+              rel="noopener noreferrer"
+              className={cx('skeleton__header__link')}
+            >
+              <FontAwesomeIcon key={1} size="lg" icon={faLinkedin} />
+            </a>,
           ]}
         />
       </header>
 
       <div className={cx('skeleton__container')}>
         <nav className={cx('skeleton__menu')}>
-          <SidebarMenu
-            menuButtonsProperties={[
-              { key: 'button1', text: 'button1', title: 'button1', icon: 'home-line', redirectPath: '/' },
-              { key: 'button2', text: 'button2', title: 'button2', icon: 'home-line', redirectPath: '/' },
-            ]}
-            menuButtonTitle="toogle menu"
-          />
+          <SidebarMenu menuButtonsProperties={[]} menuButtonTitle="toogle menu" />
         </nav>
         <main className={cx('skeleton__content')}>
           <Outlet />
